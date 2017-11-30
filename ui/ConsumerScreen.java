c4404655e8d3dc2573205270c23dae45adb54cbd
Sai Diksha <31628457+diksha2992@users.noreply.github.com>
2017-11-29 16:22:56 -0800

    Update ConsumerScreen.java

--- ui/ConsumerScreen.java
+++ ui/ConsumerScreen.java
@@ -22,34 +22,33 @@ public class ConsumerScreen {
     private JPanel controlPanel;
     private JComboBox<String> chooseDealer;
 
-
     public static void main(String[] args) throws IOException {
         ConsumerScreen  welcome = new ConsumerScreen();
-        welcome.prepareGUI();
+        welcome.createUI();
+    }
 
-        JPanel imageNestedPanel = new JPanel();
-        JLabel imageLabel = welcome.pictures();
-        imageNestedPanel.add(imageLabel);
+    public void createUI() throws IOException {
 
-        JPanel dealersListNestedPanel = new JPanel();
-        JLabel label = new JLabel("Select Dealer:",JLabel.CENTER);
-        label.setFont(new Font("Arial", Font.PLAIN, 40));
+        prepareGUI();
 
         JPanel distanceNestedPanel = new JPanel();
         JLabel distanceTextLabel = new JLabel("Show dealers within distance (mi):", JLabel.CENTER);
         distanceTextLabel.setFont(new Font("Arial", Font.PLAIN, 40));
-        JSlider slider = welcome.getSlider();
-        distanceNestedPanel.add(imageLabel);
+        JSlider slider = getSlider();
+//        distanceNestedPanel.add(imageLabel);
         distanceNestedPanel.add(distanceTextLabel);
         distanceNestedPanel.add(slider);
-        distanceNestedPanel.add(welcome.distanceLabel);
+        distanceNestedPanel.add(distanceLabel);
 
-        JComboBox<String> dealersComboBox = welcome.getDealersComboBox();
+        JPanel dealersListNestedPanel = new JPanel();
+        JLabel label = new JLabel("Select Dealer:",JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.PLAIN, 40));
+        JComboBox<String> dealersComboBox = getDealersComboBox();
         dealersListNestedPanel.add(label);
         dealersListNestedPanel.add(dealersComboBox);
 
         JPanel submitNestedPanel = new JPanel();
-        JButton submitButton = welcome.getSubmitButton();
+        JButton submitButton = getSubmitButton();
         submitNestedPanel.add(submitButton);
 
         JPanel outer = new JPanel(new GridLayout(5,1));
@@ -58,11 +57,11 @@ public class ConsumerScreen {
         outer.add(submitNestedPanel);
 
 
-        welcome.controlPanel.add(outer);
-        welcome.mainFrame.setVisible(true);
+        controlPanel.add(outer);
+        mainFrame.setVisible(true);
     }
 
-    private void prepareGUI() {
+    private void prepareGUI() throws IOException {
         mainFrame = new JFrame("Welcome User");
         mainFrame.setSize(2500,2000);
         mainFrame.setLayout(new GridLayout(3, 2));
@@ -70,13 +69,27 @@ public class ConsumerScreen {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch(Exception e) {
-            System.out.println("Error setting native LAF: " + e);
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
         }
 
+
+        JPanel imageNestedPanel = new JPanel();
+        imageNestedPanel.setLayout(new GridLayout(2,1));
+        JLabel imageLabel = pictures();
+        imageNestedPanel.add(imageLabel);
+
         headerLabel = new JLabel("Welcome to your dream car site!", JLabel.CENTER);
-        headerLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 60));
+        headerLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 100));
+
+        imageNestedPanel.add(headerLabel);
+
         statusLabel = new JLabel("", JLabel.CENTER);
         statusLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 
@@ -88,7 +101,8 @@ public class ConsumerScreen {
         controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout() );
 
-        mainFrame.add(headerLabel);
+        mainFrame.add(imageNestedPanel);
+//        mainFrame.add(headerLabel);
         mainFrame.add(controlPanel);
         mainFrame.add(statusLabel);
         mainFrame.setVisible(true);
@@ -104,7 +118,7 @@ public class ConsumerScreen {
         });
 
         submitButton.setFont(new Font("Arial", Font.PLAIN, 36));
-        submitButton.setPreferredSize(new Dimension(250, 50));
+        submitButton.setPreferredSize(new Dimension(350, 100));
         return submitButton;
     }
 
@@ -152,9 +166,9 @@ public class ConsumerScreen {
     }
     private JLabel pictures() throws IOException{
         JLabel picLabel = new JLabel(new ImageIcon("C:\\Users\\diksh\\Desktop\\image.jpg"));
-        picLabel.setSize(new Dimension(100, 100));
+//        picLabel.setSize(new Dimension(500, 200));
 
-        picLabel.setBounds(100, 200, 800, 800);
+//        picLabel.setBounds(100, 200, 2000, 800);
 
         return picLabel;
     }
